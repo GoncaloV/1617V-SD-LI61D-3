320b2cb2f915187aa08c5a46fec62483b6a7d382
André Carvalho <andre.daniel.carvalho@gmail.com>
2017-06-19 10:50:48 +0100

    Setup(again), this time correctly. Add 3 projects(server, user, interfaces) Add the microsoft service(Fully working)

---
+++ TP3/TP3/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using TP3.com.microsofttranslator.api;
+namespace TP3
+{
+    public partial class Main : Form
+    {
+        private readonly string API_KEY = ConfigurationManager.AppSettings["Microsoft_Key"];
+        private readonly SoapService MicrosoftTranslator = new SoapService();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            label1.Text = await Translate("Adeus", "pt", "en");
+        }
+
+        private async Task<String> Translate(string message, string fromLanguage, string toLanguage)
+        {
+            return await Task.Run(
+                () => MicrosoftTranslator.Translate(API_KEY, message, fromLanguage, toLanguage, "text/html", "general", "")
+            );
+        }
+
+        
+    }
+}
--- TP3/TP3/Properties/Settings.Designer.cs
+++ TP3/TP3/Properties/Settings.Designer.cs
@@ -8,22 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TP3.Properties
-{
-
-
+namespace TP3.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://api.microsofttranslator.com/V2/soap.svc")]
+        public string TP3_com_microsofttranslator_api_SoapService {
+            get {
+                return ((string)(this["TP3_com_microsofttranslator_api_SoapService"]));
             }
         }
     }
