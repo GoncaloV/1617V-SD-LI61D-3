a5d6f86d33beb0ce5327ced308c5e8753184c7f7
André Carvalho <andre.daniel.carvalho@gmail.com>
2017-06-22 23:52:05 +0100

    Forgot this ones.

--- TP3/TP3/Callbacks.cs
+++ TP3/TP3/Callbacks.cs
@@ -1,38 +1,37 @@
 ﻿using Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TP3
 {
     [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, IncludeExceptionDetailInFaults = true)]
     public class Callbacks : IUserCallback
     {
-        public List<ChatUser> userList { get; set;}
+
+        public List<ChatUser> userList { get; set; }
         private readonly object nLock = new object();
+
 
         public Callbacks()
         {
             this.userList = new List<ChatUser>();
         }
 
-        public void NotifySubscribe(ChatUser info)
+        public void NotifySubscribe(ChatUser user)
         {
             lock (nLock)
             {
-                if(userList.Find((s) => s.Binding.Equals(info.Binding)) == null)
-                    userList.Add(info);
+                if (userList.Find((u) => u.Binding.Equals(user.Binding)) == null)
+                    userList.Add(user);
             }
         }
 
-        public void NotifyUnsubscribe(ChatUser info)
+        public void NotifyUnsubscribe(ChatUser user)
         {
-
             lock (nLock)
-                userList.Remove(userList.Single(u => u.Binding.Equals(info.Binding)));
+            {
+                userList.RemoveAll(u => u.Binding.Equals(user.Binding));
+            }
         }
     }
 }
